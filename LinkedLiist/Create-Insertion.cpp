227fa83de6d5cc39f13543a1833f6099705881ea
jitender singh <jeetusinghkanyal@gmail.com>
2023-03-01 08:48:39 +0530

    insertNode at End and starting

--- LinkedLiist/Create-Insertion.cpp
+++ LinkedLiist/Create-Insertion.cpp
@@ -16,15 +16,76 @@ class NodeList{
     }
 };
 
+void insertNodeEnd(NodeList *&head, NodeList *&tail,int data){
+
+    // create a new node 
+
+    NodeList* newNode=new NodeList(data);
+
+    if(head==NULL){
+
+       head=newNode;
+       tail=newNode;
+    }
+    else{
+        tail->next=newNode;
+        tail=newNode;
+    }
+
+}
+
+
+void insertNodehead(NodeList *&head, NodeList *&tail,int data){
+
+    // create a new node 
+
+    NodeList* newNode=new NodeList(data);
+
+    if(head==NULL){
+       head=newNode;
+       tail=newNode;
+    }
+    else{
+
+        newNode->next=head;
+        head=newNode;
+    }
+
+}
+
+
+void printData(NodeList* head){
+
+    NodeList *temp=head;
+
+    while(temp!=NULL){
+
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+}
 int main(){
 
-    // NodeList* head=NULL;
-    // NodeList* tail=NULL;
+    NodeList* head=NULL;    
+    NodeList* tail=NULL;
 
 // create a new node
 
-NodeList* newNode=new NodeList(10);
+// NodeList* newNode=new NodeList(10);
 
-cout<<newNode->data;
+// cout<<newNode->data;
+
+insertNodeEnd(head,tail,40);
+insertNodeEnd(head,tail,50);
+insertNodeEnd(head,tail,60);
+insertNodeEnd(head,tail,70);
+
+insertNodehead(head,tail,40);
+insertNodehead(head,tail,30);
+insertNodehead(head,tail,20);
+insertNodehead(head,tail,10);
+
+
+printData(head);
 
 }
