7a09ee41efaa54be75f23621cd60b6bafb0ad9d9
jitender singh <jeetusinghkanyal@gmail.com>
2023-08-16 09:31:07 +0530

    day4

---
+++ Cn21days/Day4.cpp
@@ -0,0 +1,15 @@
+vector<int> printNos(int x)
+{
+
+    // Write Your Code Here
+
+    vector<int> ans;
+
+    for (int i = 1; i <= x; i++)
+    {
+
+        ans.push_back(i);
+    }
+
+    return ans;
+}
