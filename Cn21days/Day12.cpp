ed385f7b6e1be7aab18dc20ddb18052d8c6358b9
jitender singh <jeetusinghkanyal@gmail.com>
2023-08-24 12:00:42 +0530

    Day11

---
+++ Cn21days/Day12.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h> 
+
+
+
+
+void printPattern(int n) {
+
+    // Write your code here.
+
+    for(int i=1;i<=n;i++){
+
+        for(int j=1;j<=(n-i);j++)
+
+            cout<<" ";
+
+        for(int j=1;j<=2*i-1;j++)
+
+            cout<<"*";
+
+        cout<<endl;
+
+    }
+
+}
