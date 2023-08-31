336210e2402873e87bca18a9dff67b642c74f166
jitender singh <jeetusinghkanyal@gmail.com>
2023-08-31 18:05:49 +0530

    Day19

---
+++ Cn21days/Day19.cpp
@@ -0,0 +1,16 @@
+int findDuplicate(vector<int> &arr)
+{
+    // Write your code here
+    int ans = 0;
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        ans ^= arr[i];
+    }
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        ans ^= i;
+    }
+    return ans;
+}
