a0b1523e30103bf14b65ff82933d3902d92f3b41
jitender singh <jeetusinghkanyal@gmail.com>
2023-09-12 19:26:33 +0530

    Perfect Num

---
+++ Cn21days/PerfectNumber.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public:
+    int isPerfectNumber(long long N)
+    {
+        // code here
+        if (N == 1)
+            return 0;
+        long long s = 0;
+        for (long long int i = 2; i * i <= N; i++)
+        {
+            if (N % i == 0)
+            {
+                s += i;
+                if (i != N / i)
+                    s += N / i;
+            }
+        }
+        s++;
+
+        return s == N;
+    }
+};
