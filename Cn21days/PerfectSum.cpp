1d9dc18e04b5cc21b7ae528a31b8519c84598f05
jitender singh <jeetusinghkanyal@gmail.com>
2023-09-14 00:21:04 +0530

    Perfect Sum

---
+++ Cn21days/PerfectSum.cpp
@@ -0,0 +1,19 @@
+int perfectSum(int arr[], int n, int sum)
+{
+    int mod = 1000000007;
+    vector<vector<int>> dp(n + 1, vector<int>(sum + 1, 0));
+    for (int i = 0; i <= n; i++)
+        dp[i][0] = 1;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 0; j <= sum; j++)
+        {
+            if (arr[i - 1] <= j)
+                dp[i][j] = (dp[i - 1][j] + dp[i - 1][j - arr[i - 1]]) % mod;
+            else
+                dp[i][j] = dp[i - 1][j] % mod;
+        }
+    }
+    return dp[n][sum];
+}
